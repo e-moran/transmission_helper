c00e9e8d2f81d3d5b5645613b2dbd6fdbbc73e3c
Eoin Moran <eoin.t.moran00@gmail.com>
2019-04-26 18:38:02 +0100

    Rewrote the add torrent service to use data from the server configuration rather then inbuilt values

--- src/app/transmission.service.ts
+++ src/app/transmission.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {ServerConfigService} from './server-config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransmissionService {
 
-  private transmissionUrl = 'http://localhost:1337/localhost:9091/transmission/rpc';
+  private proxyUrl = 'http://localhost:1337/';
 
-  constructor(private http: HttpClient) { }
-  public addTorrent(magnet: string): Observable<any> {
-    const body = '{"arguments":{"filename":"' + magnet + '"},"method":"torrent-add"}';
-    return this.http.post(this.transmissionUrl, body);
+  constructor(private http: HttpClient, private configService: ServerConfigService) { }
+  public addTorrent(magnet: string) {
+    this.configService.getServerConfig().toPromise().then(res => {
+      const url = this.proxyUrl + res.transmissionConfig.rpcUrl;
+      const body = '{"arguments":{"filename":"' + magnet + '", "download-dir":"' + res.moviesFolder + '"},"method":"torrent-add"}';
+      this.http.post(url, body).subscribe();
+    });
   }
 }
