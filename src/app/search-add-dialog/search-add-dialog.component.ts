a36f80af34f266afd1dcb41e1c01313a1fe944cf
Eoin Moran <eoin.t.moran00@gmail.com>
2019-05-01 02:41:18 +0100

    Added file size indicator to the search add dialog

--- src/app/search-add-dialog/search-add-dialog.component.ts
+++ src/app/search-add-dialog/search-add-dialog.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { AddDialogData } from './dialog-data';
 import { FilmType } from '../serverconfig';
 import { ServerConfigService } from '../server-config.service';
+import { TransmissionService } from '../transmission.service';
 
 @Component({
   selector: 'app-search-add-dialog',
@@ -11,16 +12,24 @@ import { ServerConfigService } from '../server-config.service';
 })
 export class SearchAddDialogComponent implements OnInit {
   private selection: FilmType = null;
+  public loaded = false;
   public folders: FilmType[];
   constructor(
       public dialogRef: MatDialogRef<SearchAddDialogComponent>,
       @Inject(MAT_DIALOG_DATA) public data: AddDialogData,
-      private configApi: ServerConfigService
+      private configApi: ServerConfigService,
+      public transmissionApi: TransmissionService
   ) { }
 
   ngOnInit() {
     this.configApi.getServerConfig().subscribe(config => {
       this.folders = config.folders;
+      this.folders.forEach(folder => {
+        this.transmissionApi.getFreeSpace(folder, config).subscribe(freeSpace => {
+          folder.freeSpace = freeSpace;
+          this.loaded = true;
+        });
+      });
     });
   }
   onCancel(): void {
